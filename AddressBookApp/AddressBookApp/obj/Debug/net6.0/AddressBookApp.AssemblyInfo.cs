be12df80e74a9965501a61048346b058cc0f2230
shubham731-cmd <shubhamkr6485@gmail.com>
2025-11-25 16:14:15 +0530

    created contact class for the project

--- AddressBookApp/AddressBookApp/obj/Debug/net6.0/AddressBookApp.AssemblyInfo.cs
+++ AddressBookApp/AddressBookApp/obj/Debug/net6.0/AddressBookApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AddressBookApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af5bdec0b197b0f3013f4f84c001b22311ec497b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f751a0dd85a97c84e42b1137420c5856919f448c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AddressBookApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AddressBookApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
